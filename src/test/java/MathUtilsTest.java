c874e075f21bc35713bab812aaff44a6db86a18a
kouz <lastpuzly@gmail.com>
2020-01-13 17:12:43 +0900

    feat: Create class for testing and check junit run

---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("This test ran");
+	}
+}
